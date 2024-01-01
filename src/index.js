ab2203e5f6b3abbd65b18558d013905c0dfe6ba8
André Pereira <andrepereira97@yahoo.com>
2024-01-01 15:46:02 -0300

    :sparkles: add hapijs

--- src/index.js
+++
@@ -1,9 +0,0 @@
-const MongoDBStrategy = require('./db/strategies/mongodb.strategy');
-const ContextStrategy = require('./db/strategies/base/context.strategy');
-const PostgresStrategy = require('./db/strategies/postgres.strategy');
-
-const contextMongo = new ContextStrategy(new MongoDBStrategy());
-contextMongo.create();
-
-const contextPostgres = new ContextStrategy(new PostgresStrategy());
-contextPostgres.create();
